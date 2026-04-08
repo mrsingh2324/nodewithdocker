19579d5997ef1cf3ebad231ded1107bcd3199456
Satyam Singh <satyam.12011172@gmail.com>
2026-04-08 11:14:20 +0530

    added about

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
